41162d4edd884491d6f2ac3414d4ddd7fd54bce6
Wanjiru393 <carolciru393@gmail.com>
2023-12-14 12:48:22 +0300

    retry

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-"""0-stats.py"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-
-def print_stats(total_size, status_codes):
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-
-
-def parse_line(line):
-    try:
-        parts = line.split()
-        size = int(parts[-1])
-        code = int(parts[-2])
-        return (size, code)
-    except (TypeError):
-        return None
-
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        parsed = parse_line(line)
-        if parsed:
-            size, code = parsed
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
             total_size += size
-            if code in status_codes:
-                status_codes[code] += 1
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats(total_size, status_codes)
-except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
-    raise
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
